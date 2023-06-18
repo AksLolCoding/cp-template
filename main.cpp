778065cc22653244a8e471a0d1611f82319448a4
AksLolCoding <95454315+AksLolCoding@users.noreply.github.com>
2023-06-18 10:23:10 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<long long, long long>;
+using Vi = vector<int>;
+using VVi = vector<vector<int>>;
+using VVPi = vector<vector<pair<int, int>>>;
+using Vl = vector<long long>;
+using VVl = vector<vector<long long>>;
+
+void initIO(string name = ""){
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+	if (name.length() > 0){
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+	}
+}
+
+int main(){
+    //init
+    initIO("");
+
+    //main code
+
+    //print answer
+}
